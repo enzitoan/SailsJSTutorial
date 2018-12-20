d8df3956171288a7e0dc8a71f26b9ae12b1d5a55
enzo.ahumada <enzo.ahumada@zeke.cl>
2018-12-20 19:20:09 -0300

    initial commit

---
+++ api/controllers/UsersController.js
@@ -0,0 +1,57 @@
+/**
+ * UsersController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+  /**
+   * `UsersController.first_name()`
+   */
+  first_name: async function (req, res) {
+    return res.json({
+      todo: 'first_name() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `UsersController.last_name()`
+   */
+  last_name: async function (req, res) {
+    return res.json({
+      todo: 'last_name() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `UsersController.email()`
+   */
+  email: async function (req, res) {
+    return res.json({
+      todo: 'email() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `UsersController.phone()`
+   */
+  phone: async function (req, res) {
+    return res.json({
+      todo: 'phone() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `UsersController.address()`
+   */
+  address: async function (req, res) {
+    return res.json({
+      todo: 'address() is not implemented yet!'
+    });
+  }
+
+};
+
---
+++ api/models/Users.js
@@ -0,0 +1,38 @@
+/**
+ * Users.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    first_name: { type: 'string' },
+
+    last_name: { type: 'string' },
+
+    email: { type: 'string' },
+
+    phone: { type: 'string' },
+
+    address: { type: 'string' }
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
